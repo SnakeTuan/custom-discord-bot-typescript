0da4b0df62b66b3829bf24fb7ca2a26b309a8fb8
SnakeTuan <snaketuan@gmail.com>
2024-12-01 14:34:49 +0700

    create embed shop message

--- src/commands/execute/shop.ts
+++ src/commands/execute/shop.ts
@@ -4,14 +4,7 @@ import { getUser } from "@/utils/val-user";
 import { getShopCache } from "@/caches/shop-cache";
 import { loadSkinsList } from "@/caches/save-skin";
 import messages from "@/collections/messages";
-
-let colorMap = {
-  "0cebb8be-46d7-c12a-d306-e9907bfc5a25": 0x009984,
-  "e046854e-406c-37f4-6607-19a9ba8426fc": 0xf99358,
-  "60bca009-4182-7998-dee7-b8a2558dc369": 0xd1538c,
-  "12683d76-48d7-84a3-4e09-6985794f0445": 0x5a9fe1,
-  "411e4a55-4e59-7757-41f0-86a53f101bb5": 0xf9d563,
-};
+import { embedShopMessage } from "@/shop/embed-shop-message";
 
 export const shop = {
   name: "shop",
@@ -28,7 +21,7 @@ export const shop = {
 
     console.log("shopResult: ", shop);
 
-    if (shop.offers && shop.offers.offers.length > 0) {
+    try {
       const skinIds = shop.offers.offers.slice(0, 4); // Get the first 4 offers
 
       console.log("skinIds: ", skinIds);
@@ -38,55 +31,13 @@ export const shop = {
         await interaction.followUp("Failed to load skins data.");
         return;
       }
-      console.log(skinsData["weapons"]["eb26c879-4a83-86ad-74a5-4db9fd6b3983"]);
-      // let message = null;
-      // for (let i = 0; i < skinIds.length; i++) {
-      //   const weapon = skinsData.weapons[skinIds[i] as string];
-      //   if (!weapon) {
-      //     console.warn(`Weapon with ID ${skinIds[i]} not found in skinsData.weapons.`);
-      //     message = {
-      //       id: skinIds[i],
-      //       name: "Unknown",
-      //       image: "N/A",
-      //     };
-      //   } else {
-      //     message = {
-      //       id: skinIds[i],
-      //       name: weapon.names["es-ES"],
-      //       image: weapon.icon,
-      //     };
-      //   }
-      //   console.log("message: ", message);
-      // }
-      const skinDetails = skinIds.map((skinId: string) => {
-        console.log("skinId: ", skinId);
-        const skin = skinsData.skins[skinId];
-        if (!skin) {
-          console.warn(`Weapon with ID ${skinId} not found in skinsData.skins`);
-          return {
-            id: skinId,
-            name: "Unknown",
-            image: "N/A",
-          };
-        }
-        return {
-          id: skinId,
-          name: skin.names["es-ES"],
-          image: skin.icon,
-        };
+
+      const embed = embedShopMessage(interaction, user, shop, skinsData);
+      await interaction.followUp({
+        embeds: embed,
       });
-
-      // Create Discord embed message
-      const embed = new EmbedBuilder().setTitle("Daily Shop Offers").setColor("#0099ff").setTimestamp();
-
-      skinDetails.forEach((skin: any, index: number) => {
-        embed.addFields([
-          { name: `Offer ${index + 1}`, value: `**ID:** ${skin.id}\n**Name:** ${skin.name}\n**Image:** ${skin.image}`, inline: true },
-        ]);
-      });
-
-      await interaction.followUp({ embeds: [embed] });
-    } else {
+    } catch (e) {
+      console.error("Error fetching shop: ", e);
       await interaction.followUp("No current shop offers available.");
     }
   },
---
+++ src/shop/embed-shop-message.ts
@@ -0,0 +1,59 @@
+import { CommandInteraction, EmbedBuilder } from "discord.js";
+
+const colorMap: any = {
+  "0cebb8be-46d7-c12a-d306-e9907bfc5a25": 0x009984,
+  "e046854e-406c-37f4-6607-19a9ba8426fc": 0xf99358,
+  "60bca009-4182-7998-dee7-b8a2558dc369": 0xd1538c,
+  "12683d76-48d7-84a3-4e09-6985794f0445": 0x5a9fe1,
+  "411e4a55-4e59-7757-41f0-86a53f101bb5": 0xf9d563,
+};
+
+const remainingTime = (expireTime: number) => {
+  const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+  const remainingSeconds = expireTime - currentTime;
+  let timeLeft = "Expired";
+  if (remainingSeconds > 0) {
+    const days = Math.floor(remainingSeconds / (60 * 60 * 24));
+    const hours = Math.floor((remainingSeconds % (60 * 60 * 24)) / (60 * 60));
+    const minutes = Math.floor((remainingSeconds % (60 * 60)) / 60);
+    timeLeft = `${days}d ${hours}h ${minutes}m`;
+  }
+  return timeLeft;
+};
+
+export const embedShopMessage = (interaction: CommandInteraction, user: any, shop: any, skins: any) => {
+  console.log("Creating embed message for shop...");
+  //   console.log("expire time: ", shop.offers.expires);
+
+  const mainEmbed = new EmbedBuilder()
+    .setColor(0x1abc9c)
+    .setAuthor({
+      name: `${interaction.user.username}'s Daily Shop`,
+      iconURL: interaction.user.displayAvatarURL(),
+    })
+    .setDescription(`Expired in **${remainingTime(shop.offers.expires)}**`);
+
+  const offerItems = shop.offers.offers;
+
+  const item0Embed = new EmbedBuilder()
+    .setColor(colorMap[skins["skins"][offerItems[0]]["rarity"]])
+    .setTitle(skins["skins"][offerItems[0]]["names"]["es-ES"])
+    .setImage(skins["skins"][offerItems[0]]["icon"]);
+
+  const item1Embed = new EmbedBuilder()
+    .setColor(colorMap[skins["skins"][offerItems[1]]["rarity"]])
+    .setTitle(skins["skins"][offerItems[1]]["names"]["es-ES"])
+    .setImage(skins["skins"][offerItems[1]]["icon"]);
+
+  const item2Embed = new EmbedBuilder()
+    .setColor(colorMap[skins["skins"][offerItems[2]]["rarity"]])
+    .setTitle(skins["skins"][offerItems[2]]["names"]["es-ES"])
+    .setImage(skins["skins"][offerItems[2]]["icon"]);
+
+  const item3Embed = new EmbedBuilder()
+    .setColor(colorMap[skins["skins"][offerItems[3]]["rarity"]])
+    .setTitle(skins["skins"][offerItems[3]]["names"]["es-ES"])
+    .setImage(skins["skins"][offerItems[3]]["icon"]);
+
+  return [mainEmbed, item0Embed, item1Embed, item2Embed, item3Embed];
+};
